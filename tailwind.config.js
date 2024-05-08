c6f5a1cb4933f412f981eb650031a774c6f8b8c9
Jean-marie Carré <silentjmc@gmail.com>
2024-05-08 12:25:47 +0200

    Update color body, logo, navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
     fontFamily: {
       'title': ['lato', 'Helvetica', 'Arial', 'sans-serif']
     },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'rose-logo': '#f05166',
+      'purple-logo': '#ba74ea',     
+      'blue-logo': '#27b7e8'
+    },
     extend: {},
   },
   plugins: [
