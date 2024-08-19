1f70499b1d115286a4360dae27dd66d38da51f16
Jean-marie Carré <silentjmc@gmail.com>
2024-08-19 11:11:09 +0200

    Update PartnerType admin forms

--- src/Controller/Admin/PartnerTypeCrudController.php
+++ src/Controller/Admin/PartnerTypeCrudController.php
@@ -7,8 +7,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class PartnerTypeCrudController extends AbstractCrudController
