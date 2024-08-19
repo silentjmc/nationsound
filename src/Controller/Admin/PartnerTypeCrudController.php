fe9ef323aee528a15ddedb15cef5bcfa448917b2
Jean-marie Carré <silentjmc@gmail.com>
2024-08-19 11:09:55 +0200

    Update PartnerType admin forms

--- src/Controller/Admin/PartnerTypeCrudController.php
+++ src/Controller/Admin/PartnerTypeCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\PartnerType;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -15,14 +18,37 @@ class PartnerTypeCrudController extends AbstractCrudController
         return PartnerType::class;
     }
 
-    /*
+    public function configureActions(Actions $actions): Actions
+    {
+    return $actions
+        ->update(Crud::PAGE_INDEX, Action::NEW, function (Action $action) {
+            return $action->setLabel('Ajouter un type de partenaire');
+        })
+        ->update(Crud::PAGE_NEW, Action::SAVE_AND_RETURN, function (Action $action) {
+            return $action->setLabel('Créer le type de partenaire');
+        })
+        ->update(Crud::PAGE_NEW, Action::SAVE_AND_ADD_ANOTHER, function (Action $action) {
+            return $action->setLabel('Créer et ajouter un autre type de partenaire');
+        })
+    ;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+        ->addFormTheme('admin/form.html.twig')
+        ->setEntityLabelInSingular('Type de partenaire')
+        ->setEntityLabelInPlural('Type de partenaires')
+        ->setPageTitle('new', 'Ajouter un nouveau type de partenaire')
+            ;
+    }
+
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('type'),
         ];
     }
-    */
+    
 }
