19da85799db481b50b3e27186729377ed542edbb
Jean-marie Carré <silentjmc@gmail.com>
2024-08-20 12:43:19 +0200

    Update EventType forms

--- src/Controller/Admin/EventTypeCrudController.php
+++ src/Controller/Admin/EventTypeCrudController.php
@@ -4,8 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\EventType;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class EventTypeCrudController extends AbstractCrudController
@@ -14,15 +15,35 @@ class EventTypeCrudController extends AbstractCrudController
     {
         return EventType::class;
     }
+    public function configureActions(Actions $actions): Actions
+    {
+    return $actions
+        ->update(Crud::PAGE_INDEX, Action::NEW, function (Action $action) {
+            return $action->setLabel('Ajouter un type d\'évènement');
+        })
+        ->update(Crud::PAGE_NEW, Action::SAVE_AND_RETURN, function (Action $action) {
+            return $action->setLabel('Créer le type d\'évènement');
+        })
+        ->update(Crud::PAGE_NEW, Action::SAVE_AND_ADD_ANOTHER, function (Action $action) {
+            return $action->setLabel('Créer et ajouter un autre type d\'évènement');
+        })
+    ;
+    }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+        ->addFormTheme('admin/form.html.twig')
+        ->setEntityLabelInSingular('Type d\'évènement')
+        ->setEntityLabelInPlural('Type d\'évènements')
+        ->setPageTitle('new', 'Ajouter un nouveau type d\'évènement');
+    }
+
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('type'),
         ];
     }
-    */
 }
