140b5790499290a6dd47070b5b92ca7750befa41
Jean-marie Carré <silentjmc@gmail.com>
2024-08-20 11:11:25 +0200

    Update Artist Forms

---
+++ src/Controller/Admin/ArtistCrudController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Artist;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class ArtistCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Artist::class;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+    return $actions
+        ->update(Crud::PAGE_INDEX, Action::NEW, function (Action $action) {
+            return $action->setLabel('Ajouter un artiste');
+        })
+        ->update(Crud::PAGE_NEW, Action::SAVE_AND_RETURN, function (Action $action) {
+            return $action->setLabel('Créer un artiste');
+        })
+        ->update(Crud::PAGE_NEW, Action::SAVE_AND_ADD_ANOTHER, function (Action $action) {
+            return $action->setLabel('Créer et ajouter un autre artiste');
+        })
+    ;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+        ->addFormTheme('admin/form.html.twig')
+        ->setEntityLabelInSingular('Artiste')
+        ->setEntityLabelInPlural('Artistes')
+        ->setPageTitle('new', 'Ajouter un nouvel artiste')
+            ;
+    }
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name')->setLabel('Nom')
+            ->setFormTypeOptions([
+                'attr' => [
+                    'placeholder' => 'Saisissez le nom de l\'artiste'
+                ],
+            ]),
+            TextEditorField::new('description'),
+            ImageField::new('image')->setLabel('Image')
+                ->setUploadDir('public/uploads/artists')
+                ->setBasePath('uploads/artists')
+                ->setUploadedFileNamePattern('[name][randomhash].[extension]'),
+            TextField::new('type_music')->setLabel('Type de musique')
+                ->setFormTypeOptions([
+                    'attr' => [
+                        'placeholder' => 'Saisissez le type de musique de l\'artiste'
+                    ],
+                ]),
+        ];
+    }
+    
+}
