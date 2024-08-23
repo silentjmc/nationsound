7144078fc7cdd3d7c959e4fe8ea51e9c66b09163
Jean-marie Carré <silentjmc@gmail.com>
2024-08-23 17:39:36 +0200

    Add drawer for Privacy Policy and legal notice

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MentionsComponent } from '../mentions/mentions.component';
+import { ConfidentialiteComponent } from '../confidentialite/confidentialite.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MentionsComponent, ConfidentialiteComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent  {
+
+
 
 }
