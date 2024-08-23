fe2965bfbdb68fb4fd4a50d17ec442a860a570e5
Jean-marie Carré <silentjmc@gmail.com>
2024-08-23 13:11:13 +0200

    modify InformationComponent layout

--- src/app/information/information.component.ts
+++ src/app/information/information.component.ts
@@ -1,17 +1,37 @@
 import { Component, OnDestroy } from '@angular/core';
 import { CommonModule} from '@angular/common';
 import { Meta, Title } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMusic, faCalendar, faClock, faMapMarkerAlt, faTicketAlt, faUtensils, faHotel, faUniversalAccess, faAddressCard, faGuitar, faEarthEurope, faHandRock, faMicrophoneAlt, faHeadphones, faHeadphonesAlt, faHeadphonesSimple  } from '@fortawesome/free-solid-svg-icons';
 
 
 @Component({
   selector: 'app-information',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FontAwesomeModule],
   templateUrl: './information.component.html',
   styleUrl: './information.component.css', 
 })
 
 export class InformationComponent implements OnDestroy {
+  faMusic=faMusic
+  faCalendar=faCalendar
+  faClock=faClock
+  faMapMarkerAlt=faMapMarkerAlt
+  faTicketAlt=faTicketAlt
+  faUtensils=faUtensils
+  faHotel=faHotel
+  faUniversalAccess=faUniversalAccess
+  faAddressCard=faAddressCard 
+  faGuitar=faGuitar
+  faEarthEurope=faEarthEurope
+  faHandRock=faHandRock
+  faMicrophoneAlt=faMicrophoneAlt
+  faHeadphones=faHeadphones
+  faHeadphonesAlt=faHeadphonesAlt
+  faHeadphonesSimple=faHeadphonesSimple
+  
+
   // Information pour SEO
   constructor(private meta: Meta, private title: Title) {
     title.setTitle("Nation Sound Festival 2024 - Informations Pratiques");
