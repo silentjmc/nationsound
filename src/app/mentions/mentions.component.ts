0d076d1cfc94b9200ecf08c62905c1c0b0a796c4
Jean-marie Carré <silentjmc@gmail.com>
2024-08-23 17:39:07 +0200

    Add MentionsComponent for legal notice

---
+++ src/app/mentions/mentions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mentions',
+  standalone: true,
+  imports: [],
+  templateUrl: './mentions.component.html',
+  styleUrl: './mentions.component.css'
+})
+export class MentionsComponent {
+
+}
