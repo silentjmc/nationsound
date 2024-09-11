d97b94989b9f504be358a2541e110a2e42c83d77
Jean-marie Carré <silentjmc@gmail.com>
2024-09-11 11:35:24 +0200

    Add unique email verification

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,10 +4,14 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(
+        fields:'email',
+        message:'l\'émail est déjà utilisé')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     public function getRoles(): array
