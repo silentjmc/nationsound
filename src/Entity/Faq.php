9421dab9f6d62c79699daf086202ad5b9ecb96e3
Jean-marie Carré <silentjmc@gmail.com>
2024-09-13 16:22:47 +0200

    Add FAQ Entity and CRUD operations

---
+++ src/Entity/Faq.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FaqRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FaqRepository::class)]
+class Faq
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $question = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $reponse = null;
+
+    #[ORM\Column]
+    private ?bool $publier = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getQuestion(): ?string
+    {
+        return $this->question;
+    }
+
+    public function setQuestion(string $question): static
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+
+    public function getReponse(): ?string
+    {
+        return $this->reponse;
+    }
+
+    public function setReponse(string $reponse): static
+    {
+        $this->reponse = $reponse;
+
+        return $this;
+    }
+
+    public function isPublier(): ?bool
+    {
+        return $this->publier;
+    }
+
+    public function setPublier(bool $publier): static
+    {
+        $this->publier = $publier;
+
+        return $this;
+    }
+}
