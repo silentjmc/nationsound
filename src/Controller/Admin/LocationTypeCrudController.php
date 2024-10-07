444a398df73dc098c2b022bd7f979904dd47796b
Jean-marie Carré <silentjmc@gmail.com>
2024-10-07 16:13:24 +0200

    update CRUD structure

--- src/Controller/Admin/LocationTypeCrudController.php
+++ src/Controller/Admin/LocationTypeCrudController.php
@@ -2,16 +2,28 @@
 
 namespace App\Controller\Admin;
 
+//use App\Entity\EntityHistory;
+
 use App\Entity\LocationType;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 
 class LocationTypeCrudController extends AbstractCrudController
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+
+    }
+
     public static function getEntityFqcn(): string
     {
         return LocationType::class;
@@ -61,15 +73,34 @@ class LocationTypeCrudController extends AbstractCrudController
         ->showEntityActionsInlined();
     }
 
-    public function configureFields(string $pageName): iterable
+    public mixed $historyRepository = true;
+
+    public function configureFields(string $pageName): iterable 
     {
-        return [
+        $fields = [
             TextField::new('type','Type de lieu'),
-            ImageField::new('symbol','Télécharger le symbole représentant le lieu sur la carte')
+            ImageField::new('symbol',($pageName === Crud::PAGE_INDEX ? 'symbole' : 'Télécharger le symbole représentant le lieu sur la carte'))
                 ->setUploadDir('public/uploads/locations')
                 ->setBasePath('uploads/locations')
-                ->setUploadedFileNamePattern('[name][randomhash].[extension]'),
-        ];
+                ->setUploadedFileNamePattern('[name][randomhash].[extension]')
+                ->setFormTypeOptions([
+                    'required' => ($pageName === Crud::PAGE_NEW ? true : false),
+                ]),
+                DateTimeField::new('dateModification', 'Dernière modification')->onlyOnIndex(),
+                TextField::new('userModification', 'Utilisateur')->onlyOnIndex()
+        ];       
+        return $fields;
     }
-    
+
+
 }
+
+
+
+
+
+
+
+
+
+
--- src/Controller/Admin/PartnerTypeCrudController.php
+++ src/Controller/Admin/PartnerTypeCrudController.php
@@ -3,7 +3,6 @@
 namespace App\Controller\Admin;
 
 use App\Entity\PartnerType;
-use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
