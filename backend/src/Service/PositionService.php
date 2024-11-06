08de164e81cda79e736f7ef4cec5244d2d730383
Jean-marie Carré <silentjmc@gmail.com>
2024-11-06 14:30:09 +0100

    Service for order some entities

---
+++ backend/src/Service/PositionService.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Service;
+
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
+
+enum Direction
+{
+    case Top;
+    case Up;
+    case Down;
+    case Bottom;
+}
+
+class PositionService
+{
+    public function __construct(
+        private readonly EntityManagerInterface $em
+    ) {
+    }
+    public function move(AdminContext $context, Direction $direction)
+    {
+        $object = $context->getEntity()->getInstance();
+        $newPosition = match($direction) {
+            Direction::Top => 0,
+            Direction::Up => $object->getPosition() - 1,
+            Direction::Down => $object->getPosition() + 1,
+            Direction::Bottom => -1,
+        };
+    
+        $object->setPosition($newPosition);
+        $this->em->flush();
+    }
+}
