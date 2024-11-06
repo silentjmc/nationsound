10149ac1a01ba3257dde2c52795572c5339cc8ab
Jean-marie Carré <silentjmc@gmail.com>
2024-11-06 21:52:23 +0100

    Modify API connexion

--- frontend/src/app/services/map.service.ts
+++ frontend/src/app/services/map.service.ts
@@ -1,17 +1,41 @@
-import { Injectable, PLATFORM_ID, Inject } from '@angular/core';
+import { Injectable, PLATFORM_ID, Inject, inject } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
+import { HttpClient} from '@angular/common/http';
+import { Observable, map, shareReplay,} from 'rxjs';
+import { Poi } from './class';
+import { environment } from '../../environments/environment';
 
+const BASE_URL = 'http://127.0.0.1:8000/api/eventLocation'
 @Injectable()
 export class MapService {
+  private http = inject(HttpClient)
+  poiEa$!: Observable<Poi[]>;
 
-  public L: any;
-  public leafletLoaded: Promise<any> = Promise.resolve();
-  
-  // Chargement de la librairie Leaflet
-  // Loading the Leaflet library
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.poiEa$ = this.getLocation();
     if (isPlatformBrowser(platformId)) {
       this.leafletLoaded = import('leaflet').then(module => this.L = module.default);
     }
   }
+
+  getLocation():Observable<Poi[]> {
+    return this.http.get<Poi[]>(BASE_URL).pipe(
+      map((response: any[]) => {
+        return response.map(event => ({
+          id: event.id,
+          name: event.locationName,
+          type: event.typeLocation.type,
+          text: event.description,
+          lat: event.latitude,
+          lon: event.longitude,
+          iconUrl: `${environment.apiUrl}/uploads/locations/${event.typeLocation.symbol}`
+        }));
+      }),
+      shareReplay(1)
+    );
+  }
+
+  public L: any;
+  public leafletLoaded: Promise<any> = Promise.resolve();
+    
 }
