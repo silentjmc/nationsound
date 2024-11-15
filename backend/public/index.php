80765785480e5632919bda764e0f4a4cd60fceed
Jean-marie Carré <silentjmc@gmail.com>
2024-11-15 09:29:52 +0100

    inform dirname for dev ou prod

--- backend/public/index.php
+++ backend/public/index.php
@@ -4,7 +4,11 @@ use App\Kernel;
 
 date_default_timezone_set('Europe/Paris');
 
+## chemin vers le fichier autoload.php en local
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+## chemin vers le fichier autoload.php de symfony sur le serveur
+#require_once dirname(__DIR__).'/symfony/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
