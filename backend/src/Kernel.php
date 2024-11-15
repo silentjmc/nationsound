5c10c2d819a52e513fad02259fd4051db3693eae
Jean-marie Carré <silentjmc@gmail.com>
2024-11-15 09:32:28 +0100

    clean code

--- backend/src/Kernel.php
+++ backend/src/Kernel.php
@@ -2,11 +2,11 @@
 
 namespace App;
 
-use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-
+   
 }
