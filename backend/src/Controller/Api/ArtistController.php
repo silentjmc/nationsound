8d51adbebdc0df72b5fd3d58ec343ccc2b01f954
Jean-marie Carré <silentjmc@gmail.com>
2024-11-23 22:49:07 +0100

    Change api route

--- backend/src/Controller/Api/ArtistController.php
+++ backend/src/Controller/Api/ArtistController.php
@@ -42,7 +42,7 @@ class ArtistController extends AbstractController
             $jsonArtistList, Response::HTTP_OK, [], true);
     }*/
             
-    #[Route('/api/artist', name: 'app_api_artist', methods: ['GET'])]
+    #[Route('/api/artistsList', name: 'app_api_artistsList', methods: ['GET'])]
     public function getArtistList(ArtistRepository $artistRepository, SerializerInterface $serializer): JsonResponse
     {
         $artistList = $artistRepository->findAll();
@@ -58,5 +58,26 @@ class ArtistController extends AbstractController
 
         $jsonArtistList = $serializer->serialize(array_values($artistsWithPublishedEvents), 'json', ['groups' => 'getArtist']);
         return new JsonResponse($jsonArtistList, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/artist/{id}', name: 'app_api_artist', methods: ['GET'])]
+    public function getArtist(int $id, ArtistRepository $artistRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $artist = $artistRepository->find($id);
+        
+        if ($artist) {
+            // Filtrer les événements publiés
+            $artist->publishedEventsLinked();
+
+            // Ne retourner l'artiste que s'il a des événements publiés
+            if (count($artist->getEvents()) === 0) {
+                return new JsonResponse(['message' => 'Aucun événement publié pour cet artiste'], Response::HTTP_NOT_FOUND);
+            }
+
+            $jsonArtist = $serializer->serialize($artist, 'json', ['groups' => 'getArtist']);
+            return new JsonResponse($jsonArtist, Response::HTTP_OK, [], true);
+        } else {
+            return new JsonResponse(['message' => 'Artiste non trouvé'], Response::HTTP_NOT_FOUND);
+        }
     }
 }
