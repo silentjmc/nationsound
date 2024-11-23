679593c990308a71bd860e06b89719ef8adbb3f6
Jean-marie Carré <silentjmc@gmail.com>
2024-11-23 22:27:51 +0100

    Add news detail page

---
+++ frontend/src/app/news-detail/news-detail.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { News } from '../services/class';
+import { ActivatedRoute } from '@angular/router';
+import { NewsService } from '../services/news.service';
+import { CommonModule, Location } from '@angular/common';
+
+@Component({
+  selector: 'app-news-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './news-detail.component.html',
+  styleUrl: './news-detail.component.css'
+})
+export class NewsDetailComponent {
+  news$!: Observable<News>;
+  loading$ = new BehaviorSubject<boolean>(true);
+  error$ = new BehaviorSubject<boolean>(false);
+
+  constructor(private route: ActivatedRoute,private newsService: NewsService, private location: Location)
+   {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    if (id) {
+      this.loading$.next(true);
+      this.error$.next(false);
+      
+      this.newsService.getNewsById(id).subscribe({
+        next: (news) => {
+          if (news) {
+            this.news$ = new Observable(observer => {
+              observer.next(news);
+              observer.complete();
+            });
+          } else {
+            this.error$.next(true);
+          }
+          this.loading$.next(false);
+        },
+        error: () => {
+          this.error$.next(true);
+          this.loading$.next(false);
+        }
+      });
+    } else {
+      this.error$.next(true);
+      this.loading$.next(false);
+    }
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+}
