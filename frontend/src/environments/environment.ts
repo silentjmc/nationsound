ededdd4f9b3ed6e235328036984a2831c5e88eba
Jean-marie Carré <silentjmc@gmail.com>
2024-11-23 22:40:28 +0100

    Change url for local url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production : false,
-    apiUrl: 'https://jmcarre.go.yj.fr/admin'
+    //apiUrl: 'https://jmcarre.go.yj.fr/admin'
+    apiUrl: 'http://127.0.0.1:8000'
 };
