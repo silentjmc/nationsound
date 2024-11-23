4bb89f2329d7e8c13e7cb75d621a7ac16d9cfc29
Jean-marie Carré <silentjmc@gmail.com>
2024-11-23 22:34:58 +0100

    Update config

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,10 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
+//import 'babel-polyfill';
+//import Pushy from 'pushy-sdk-web';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch())]
+  //providers: [provideRouter(routes), provideHttpClient(withFetch()), provideServiceWorker('ngsw-worker.js', {
+  providers: [provideRouter(routes), provideHttpClient(withFetch()), provideServiceWorker('service-worker.js', {
+        //enabled: !isDevMode(),
+        enabled:true, 
+        registrationStrategy: 'registerWhenStable:30000'
+    })]
 };
