82cb129e9e8645bf7bf482751f7fd26b683ca64b
Jean-marie Carré <silentjmc@gmail.com>
2024-11-23 22:28:27 +0100

    Add news link

--- frontend/src/app/nav-bar-informations/nav-bar-informations.component.ts
+++ frontend/src/app/nav-bar-informations/nav-bar-informations.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 import { InformationComponent } from '../information/information.component';
 import { FaqComponent } from '../faq/faq.component';
+import { NewsSummaryComponent } from '../news-summary/news-summary.component';
 
 @Component({
   selector: 'app-nav-bar-informations',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, RouterModule, InformationComponent, FaqComponent],
+  imports: [RouterLink, RouterLinkActive, RouterModule, InformationComponent, FaqComponent, NewsSummaryComponent],
   templateUrl: './nav-bar-informations.component.html',
   styleUrl: './nav-bar-informations.component.css'
 })
