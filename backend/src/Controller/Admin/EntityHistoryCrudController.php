13e926c698762484baed7cd49637373745e32933
Jean-marie Carré <silentjmc@gmail.com>
2024-11-27 18:49:48 +0100

    Add CRUD configuration

--- backend/src/Controller/Admin/EntityHistoryCrudController.php
+++ backend/src/Controller/Admin/EntityHistoryCrudController.php
@@ -3,11 +3,15 @@
 namespace App\Controller\Admin;
 
 use App\Entity\EntityHistory;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
 
 class EntityHistoryCrudController extends AbstractCrudController
 {
@@ -16,17 +20,41 @@ class EntityHistoryCrudController extends AbstractCrudController
         return EntityHistory::class;
     }
 
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+        ->disable('new', 'edit', 'delete')
+        ->add(Crud::PAGE_INDEX, Action::DETAIL)
+        ->update(Crud::PAGE_INDEX, Action::DETAIL, function (Action $action) {
+            return $action
+                ->setIcon('fa fa-magnifying-glass')
+                ->setLabel(false)
+                ->setHtmlAttributes([
+                    'title' => 'Modifier cet élément',
+                ])
+                ->displayAsLink()
+                ->addCssClass('btn btn-sm btn-light');
+        });
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->showEntityActionsInlined()
+            ->setPageTitle('index', 'Historique des modifications')
+            ->setPageTitle('detail', 'Détails de la modification');
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
             TextField::new('entityName','Nom'),
             IntegerField::new('entityId','Identificateur'),
             TextField::new('action'),
-            TextField::new('user.fullname','Utilisateur'),
-            DateTimeField::new('dateAction','Date'),
-            ArrayField::new('oldValuesAsString', 'Anciennes valeurs')->onlyOnIndex(),
-            ArrayField::new('newValuesAsString', 'Nouvelles valeurs')->onlyOnIndex(),
-            
+            TextField::new('user','Utilisateur'),
+            DateTimeField::new('dateAction','Date de modification'),
+            ArrayField::new('oldValues', 'Anciennes valeurs'),
+            ArrayField::new('newValues', 'Nouvelles valeurs'),
         ];
     }
 }
