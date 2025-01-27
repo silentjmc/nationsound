51e09a28bf4d544afce363bd5a879d7e6e99a829
Jean-marie Carré <silentjmc@gmail.com>
2025-01-27 11:09:42 +0100

    Add load spinner

--- frontend/src/app/carousel/carousel.component.ts
+++ frontend/src/app/carousel/carousel.component.ts
@@ -1,7 +1,7 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA,OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArtistService } from '../services/artist.service';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, catchError, EMPTY, Observable } from 'rxjs';
 import { Artist } from '../services/class';
 import { RouterLink, RouterModule } from '@angular/router';
 
@@ -17,11 +17,25 @@ import { RouterLink, RouterModule } from '@angular/router';
 
 export class CarouselComponent implements OnInit  {
   artists$!: Observable<Artist[]>;
+  loading$ = new BehaviorSubject<boolean>(true);
+  error$ = new BehaviorSubject<boolean>(false);
   constructor(private artistService: ArtistService) {
   }
 
   ngOnInit(): void {
+    this.loading$.next(true);
+    this.error$.next(false);
     //  Get artist data from the service
-    this.artists$ = this.artistService.getAllArtists();
+    //this.artists$ = this.artistService.getAllArtists();
+    this.artists$ = this.artistService.getAllArtists().pipe(
+      catchError(() => {
+        this.error$.next(true);
+        this.loading$.next(false);
+        return EMPTY; 
+      })
+    );
+    this.artists$.subscribe(() => {
+      this.loading$.next(false); 
+    });
   }
 }
