5c95843cf6537f171558532af9a0c9dabd5d8850
Jean-marie Carré <silentjmc@gmail.com>
2025-01-27 11:16:30 +0100

    Update partners service with Api partners

--- frontend/src/app/services/partnerswp.service.ts
+++ frontend/src/app/services/partnerswp.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { map, Observable } from 'rxjs';
+import { Partner } from './class';
 
 const BASE_URL = `${environment.apiUrl}/api/partners`;
 @Injectable({
@@ -8,9 +10,21 @@ const BASE_URL = `${environment.apiUrl}/api/partners`;
 })
 export class PartnerswpService {
   private http = inject(HttpClient)
+  partners$!: Observable<Partner[]>; 
   constructor() { }
   // Getting partners
-  getPosts() {
+  /*getPosts() {
     return this.http.get(BASE_URL);
+  }*/
+
+  getPartners(): Observable<Partner[]> {
+    return this.http.get<Partner[]>(BASE_URL).pipe(
+      map((partners: Partner[]) => {
+        return partners.map(partner => ({
+          ...partner,
+          image: `${environment.apiUrl}/uploads/partners/${partner.image}`
+        }));
+      })
+    );
   }
 }
