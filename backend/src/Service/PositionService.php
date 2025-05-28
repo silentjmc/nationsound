b00b51bd251a54ec6a43eb2783594d7ea1dc1936
Jean-marie Carré <silentjmc@gmail.com>
2025-05-28 15:09:26 +0200

    Update name field & add method for specified fieldname for each table

--- backend/src/Service/PositionService.php
+++ backend/src/Service/PositionService.php
@@ -3,6 +3,7 @@ namespace App\Service;
 
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
+use Symfony\Component\HttpFoundation\RequestStack; //rajout
 
 enum Direction
 {
@@ -15,20 +16,66 @@ enum Direction
 class PositionService
 {
     public function __construct(
-        private readonly EntityManagerInterface $entityManager
+        private readonly EntityManagerInterface $entityManager,
+        private readonly RequestStack $requestStack //rajout
     ) {
     }
-    public function move(AdminContext $context, Direction $direction)
+
+    // Generates method names based on the entity's class name.
+    private function getMethodNames(object $entity): array
+    {
+        $entityName = (new \ReflectionClass($entity))->getShortName();
+        
+        return [
+            'setPosition' => 'setPosition' . $entityName,
+            'getPosition' => 'getPosition' . $entityName,
+        ];
+    }
+   
+    // Moves the entity to a new position based on the specified direction.
+    public function move(AdminContext $context, Direction $direction): array
     {
         $object = $context->getEntity()->getInstance();
-        $newPosition = match($direction) {
-            Direction::Top => 0,
-            Direction::Up => $object->getPosition() - 1,
-            Direction::Down => $object->getPosition() + 1,
-            Direction::Bottom => -1,
-        };
-    
-        $object->setPosition($newPosition);
+        $methods = $this->getMethodNames($object);
+        $getter = $methods['getPosition'];
+        $setter = $methods['setPosition'];
+
+        $currentPosition = $object->$getter();
+        $message = '';
+        $newPosition = $currentPosition;
+        
+        switch ($direction) {
+            case Direction::Top:
+                $newPosition = 0;
+                $message = 'l\'élément a bien été déplacé en haut de page.';  
+                break;
+                
+            case Direction::Up:
+                $newPosition = $currentPosition - 1;
+                $message = 'l\'élément a bien été déplacé d\'un cran en haut.';
+                break;
+                
+            case Direction::Down:
+                $newPosition = $currentPosition + 1;
+                $message = 'l\'élément a bien été déplacé d\'un cran en bas.';
+                break;
+                
+            case Direction::Bottom:
+                $newPosition = -1;
+                $message = 'l\'élément a bien été déplacé en bas de page.';
+                break;
+        }
+        
+        // Update the position of the current object
+
+        $object->$setter($newPosition);
+        $this->entityManager->persist($object);
         $this->entityManager->flush();
+        
+        return [
+            'success' => true,
+            'message' => $message,
+            'redirect_url' => $this->requestStack->getCurrentRequest()->headers->get('referer')
+        ];
     }
 }
