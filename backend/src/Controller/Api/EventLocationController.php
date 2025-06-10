8a2571d4d7d3f24c021e2a98597b3e9f7042d1c6
silentjmc <148895309+silentjmc@users.noreply.github.com>
2025-06-10 09:17:19 +0200

    Update request filter on publish item

--- backend/src/Controller/Api/EventLocationController.php
+++ backend/src/Controller/Api/EventLocationController.php
@@ -14,7 +14,8 @@ class EventLocationController extends AbstractController
     #[Route('/api/eventLocation', name: 'app_eventLocation', methods: ['GET'])]
     public function getEventLocationList(EventLocationRepository $eventLocationRepository, SerializerInterface $serializer): JsonResponse
     {
-        $eventLocationList = $eventLocationRepository->findAll();
+        $eventLocationList = $eventLocationRepository->findBy(
+            ['publishEventLocation' => true]);  
         $jsonEventLocationList = $serializer->serialize($eventLocationList, 'json',['groups' => 'getEventLocation']);
         return new JsonResponse(
             $jsonEventLocationList, Response::HTTP_OK, [], true);
--- backend/src/Controller/Api/PartnersController.php
+++ backend/src/Controller/Api/PartnersController.php
@@ -14,7 +14,8 @@ class PartnersController extends AbstractController
     #[Route('/api/partners', name: 'app_partners', methods: ['GET'])]
     public function getPartnersList(PartnerRepository $partnersRepository, SerializerInterface $serializer): JsonResponse
     {
-        $partnersList = $partnersRepository->findAll();
+        $partnersList = $partnersRepository->findBy(
+            ['publishPartner' => true]);
         $jsonPartnersList = $serializer->serialize($partnersList, 'json',['groups' => 'getPartners']);
         return new JsonResponse(
             $jsonPartnersList, Response::HTTP_OK, [], true);
